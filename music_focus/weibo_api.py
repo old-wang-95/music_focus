3d189d02fc73f3088b28e9291e7ffe47a39d9768
brown <wangjiang@datagrand.com>
2020-01-24 20:06:09 +0800

    add get_posts_by_user api

--- music_focus/weibo_api.py
+++ music_focus/weibo_api.py
@@ -2,15 +2,24 @@ import json
 
 import requests
 
+from music_focus.beans.post import Post
 from music_focus.beans.user import User, Gender
 from music_focus.cache import Cache
+from music_focus.utils import dt_utils
 
 
 def get_user_info(user_id):
+    """
+    根据用户id, 获取用户相关信息
+
+    :param user_id:
+    :type user_id: int
+    :return:
+    """
     url = 'https://m.weibo.cn/api/container/getIndex?type=uid&value={}'.format(user_id)
     res = Cache().cache(url, requests.get, url)
     assert res.status_code == 200, \
-        'get user_info fail, status code: {}, text: {}'.format(res.status_code, res.text)
+        'get {} user_info fail, status code: {}, text: {}'.format(user_id, res.status_code, res.text)
 
     data = json.loads(res.text)
     user = User(
@@ -24,7 +33,7 @@ def get_user_info(user_id):
         profile=int(
             [tab['containerid'] for tab in data['data']['tabsInfo']['tabs'] if tab['tab_type'] == 'profile'][0]
         ),
-        weibo=int(
+        posts=int(
             [tab['containerid'] for tab in data['data']['tabsInfo']['tabs'] if tab['tab_type'] == 'weibo'][0]
         ),
         video=int(
@@ -38,3 +47,37 @@ def get_user_info(user_id):
         )
     )
     return user
+
+
+def get_posts_by_user(user):
+    """
+    根据user, 获取用户的微博列表
+
+    :param user:
+    :type user: User
+    :return:
+    """
+    url = 'https://m.weibo.cn/api/container/getIndex?type=uid&value={}&containerid={}'.format(user.id, user.posts)
+    res = Cache().cache(url, requests.get, url)
+    assert res.status_code == 200, \
+        'get {} posts fail, status code: {}, text: {}'.format(user, res.status_code, res.text)
+
+    posts = []
+    data = json.loads(res.text)
+    for card in data['data']['cards']:
+        if card['card_type'] != 9:
+            continue
+        post = Post(
+            id=int(card['mblog']['id']),
+            user_id=user.id,
+            user_name=user.name,
+            time=dt_utils.parse_date_time(card['mblog']['created_at']),
+            content=card['mblog']['text'],
+            share_cnt=card['mblog']['reposts_count'],
+            comment_cnt=card['mblog']['comments_count'],
+            like_cnt=card['mblog']['attitudes_count'],
+            comments=[]
+        )
+        posts.append(post)
+
+    return posts
--- tests/test_weibo_api.py
+++ tests/test_weibo_api.py
@@ -1,6 +1,7 @@
 import unittest
 
 from music_focus import weibo_api
+from music_focus.beans.user import User, Gender
 
 
 class Test(unittest.TestCase):
@@ -10,7 +11,15 @@ class Test(unittest.TestCase):
 
     def test_get_user_info(self):
         print('test_get_user_info~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-        user = weibo_api.get_user_info('1613258127')
+        user = weibo_api.get_user_info(1613258127)
+        print(user)
+
+    def test_get_posts_by_user(self):
+        print('test_get_posts_by_user~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        user = User(id=1613258127, name='宋冬野',
+                    gender=Gender.male, verified=True, description='', followers_cnt=0, follow_cnt=0,
+                    profile=0, posts=1076031613258127, video=0, super_topic=0, album=0)
+        user = weibo_api.get_posts_by_user(user)
         print(user)
 
     def tearDown(self):
