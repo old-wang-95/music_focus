cd4cf710e42ebae8163b86f5251a86c4fc087ef5
brown <wangjiang@datagrand.com>
2020-01-24 15:45:11 +0800

    optimize user bean

--- music_focus/beans/user.py
+++ music_focus/beans/user.py
@@ -9,15 +9,21 @@ class Gender(Enum):
 
 @dataclass
 class User:
+    """
+    每种tab [profile, weibo, video, super_topic, album], 都是一个container
+    sina都会有一个container id
+    """
+
     id: int
     name: str
     gender: Gender
-    verified: bool  # 是否被验证
-    description: str  # 简介
-    followers_cnt: int  # 关注者数量
-    follow_cnt: int  # 关注数量
-    profile: int  # 主页id
-    weibo: int  # 微博页id
-    video: int  # 视频页id
-    super_topic: int  # 超话页id
-    album: int  # 相册页id
+    verified: bool
+    description: str
+    followers_cnt: int
+    follow_cnt: int
+
+    profile: int
+    weibo: int
+    video: int
+    super_topic: int
+    album: int
