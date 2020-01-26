1450d667cb42a11aaeae1b8f3660ba2143d9f9e8
brown <wangjiang@datagrand.com>
2020-01-27 01:43:46 +0800

    add empty focuses_result_processor and tests

---
+++ music_focus/processors/transform_focuses_result_processor.py
@@ -0,0 +1,7 @@
+from music_focus.processors.processor_base import ProcessorBase
+
+
+class TransformFocusesResultProcessor(ProcessorBase):
+
+    def run(self, workflow_input, tmp_result, workflow_output):
+        pass
