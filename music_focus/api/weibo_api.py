762f9521491665f0de613905b7c8c436b3d41827
brown <wangjiang@datagrand.com>
2020-01-26 19:43:14 +0800

    fix index bug && add use_cache in params

--- music_focus/api/weibo_api.py
+++ music_focus/api/weibo_api.py
@@ -4,24 +4,36 @@ import requests
 
 from music_focus.beans.post import Post
 from music_focus.beans.user import User, Gender
-from music_focus.utils.cache import Cache
 from music_focus.utils import dt_utils
+from music_focus.utils.cache import Cache
 
 
-def get_user_info(user_id):
+def get_user_info(user_id, use_cache=True):
     """
     根据用户id, 获取用户相关信息
 
     :param user_id:
     :type user_id: int
+    :param use_cache:
+    :type use_cache: bool
     :return:
     """
     url = 'https://m.weibo.cn/api/container/getIndex?type=uid&value={}'.format(user_id)
-    res = Cache().cache(url, requests.get, url)
+    if use_cache:
+        res = Cache().cache(url, requests.get, url)
+    else:
+        res = requests.get(url)
     assert res.status_code == 200, \
         'get {} user_info fail, status code: {}, text: {}'.format(user_id, res.status_code, res.text)
 
     data = json.loads(res.text)
+
+    def _get_tab_type(tab_type):
+        for tab in data['data']['tabsInfo']['tabs']:
+            if tab['tab_type'] == tab_type:
+                return int(tab['containerid'])
+        return 0
+
     user = User(
         id=user_id,
         name=data['data']['userInfo']['screen_name'],
@@ -30,35 +42,30 @@ def get_user_info(user_id):
         description=data['data']['userInfo']['description'],
         followers_cnt=data['data']['userInfo']['followers_count'],
         follow_cnt=data['data']['userInfo']['follow_count'],
-        profile=int(
-            [tab['containerid'] for tab in data['data']['tabsInfo']['tabs'] if tab['tab_type'] == 'profile'][0]
-        ),
-        posts=int(
-            [tab['containerid'] for tab in data['data']['tabsInfo']['tabs'] if tab['tab_type'] == 'weibo'][0]
-        ),
-        video=int(
-            [tab['containerid'] for tab in data['data']['tabsInfo']['tabs'] if tab['tab_type'] == 'video'][0]
-        ),
-        super_topic=int(
-            [tab['containerid'] for tab in data['data']['tabsInfo']['tabs'] if tab['tab_type'] == 'cardlist'][0]
-        ),
-        album=int(
-            [tab['containerid'] for tab in data['data']['tabsInfo']['tabs'] if tab['tab_type'] == 'album'][0]
-        )
+        profile=_get_tab_type('profile'),
+        posts=_get_tab_type('weibo'),
+        video=_get_tab_type('video'),
+        super_topic=_get_tab_type('cardlist'),
+        album=_get_tab_type('album')
     )
     return user
 
 
-def get_posts_by_user(user):
+def get_posts_by_user(user, use_cache=True):
     """
     根据user, 获取用户的微博列表
 
     :param user:
     :type user: User
+    :param use_cache:
+    :type use_cache: bool
     :return:
     """
     url = 'https://m.weibo.cn/api/container/getIndex?type=uid&value={}&containerid={}'.format(user.id, user.posts)
-    res = Cache().cache(url, requests.get, url)
+    if use_cache:
+        res = Cache().cache(url, requests.get, url)
+    else:
+        res = requests.get(url)
     assert res.status_code == 200, \
         'get {} posts fail, status code: {}, text: {}'.format(user, res.status_code, res.text)
 
