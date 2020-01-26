d4bdbdd148d467cc19553cd68decf26c4dc5dedf
brown <wangjiang@datagrand.com>
2020-01-27 02:27:11 +0800

    bug fix

--- music_focus/processors/rank_processor.py
+++ music_focus/processors/rank_processor.py
@@ -8,11 +8,12 @@ class RankProcessor(ProcessorBase):
         if 'posts' in tmp_result:
             posts = tmp_result['posts']
             for music_type, each_posts in posts.items():
-                each_sorted_posts = [post for _, post in sorted(zip(scores[music_type], each_posts), reverse=True)]
+                each_sorted_posts = [post for _, post in
+                                     sorted(zip(scores[music_type], each_posts), key=lambda t: t[0], reverse=True)]
                 posts[music_type] = each_sorted_posts
         elif 'focuses' in tmp_result:
             focuses = tmp_result['focuses']
             for music_type, each_focuses in focuses.items():
                 each_sorted_focuses = [focus for _, focus in
-                                       sorted(zip(scores[music_type], each_focuses), reverse=True)]
+                                       sorted(zip(scores[music_type], each_focuses), key=lambda t: t[0], reverse=True)]
                 focuses[music_type] = each_sorted_focuses
