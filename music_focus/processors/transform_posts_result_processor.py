9148fbef3ef853f16d5dbf258c3dd7523481ce90
brown <wangjiang@datagrand.com>
2020-01-27 01:25:34 +0800

    add transform_posts_result_processor and its test

---
+++ music_focus/processors/transform_posts_result_processor.py
@@ -0,0 +1,23 @@
+from music_focus.processors.processor_base import ProcessorBase
+
+
+class TransformPostsResultProcessor(ProcessorBase):
+
+    def run(self, workflow_input, tmp_result, workflow_output):
+        posts_result = workflow_input['posts']
+        json_result = {}
+        for music_type, each_posts in posts_result['posts'].items():
+            json_result[music_type] = []
+            for idx, each_post in enumerate(each_posts):
+                json_result[music_type].append({
+                    'id': each_post.id,
+                    'user_id': each_post.user_id,
+                    'user_name': each_post.user_name,
+                    'time': str(each_post.time),
+                    'content': each_post.content,
+                    'share_cnt': each_post.share_cnt,
+                    'comment_cnt': each_post.comment_cnt,
+                    'like_cnt': each_post.like_cnt,
+                    'scores': posts_result['scores'][music_type][idx]
+                })
+        workflow_output['result'] = json_result
---
+++ tests/test_processors/test_transform_posts_result.py
@@ -0,0 +1,39 @@
+import unittest
+from datetime import datetime
+import json
+
+from music_focus.beans.post import Post
+from music_focus.processors.transform_posts_result_processor import TransformPostsResultProcessor
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test(self):
+        workflow_input = {
+            'posts': {
+                'scores': {
+                    'rock': [7000, 348680]},
+                'posts': {
+                    'rock': [
+                        Post(id=1, user_id=1, user_name='痛仰乐队', time=datetime(2020, 1, 24, 0, 0), content='',
+                             share_cnt=1, comment_cnt=1, like_cnt=1, comments=[]),
+                        Post(id=2, user_id=2, user_name='老王乐队', time=datetime(2020, 1, 25, 0, 0), content='',
+                             share_cnt=2, comment_cnt=2, like_cnt=2, comments=[])
+                    ]
+                }
+            }
+        }
+        workflow_output = {}
+        p = TransformPostsResultProcessor()
+        p.run(workflow_input, {}, workflow_output)
+        print(json.dumps(workflow_output['result'], ensure_ascii=False, indent=2))
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
