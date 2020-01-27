e4b4d9b17e07459c64bde06699a28804a841250b
brown <wangjiang@datagrand.com>
2020-01-27 19:56:42 +0800

    add focus bean

---
+++ music_focus/beans/focus.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Focus:
+    title: str
+    description: str
+    today_read: int  # 今日阅读数
+    read_cnt: int  # 总阅读
+    discus_cnt: int  # 总讨论数
+    member_cnt: int  # 总参与人数
+    link: str
