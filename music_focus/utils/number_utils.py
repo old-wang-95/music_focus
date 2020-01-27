18b66c1dc6ccaacfb4a423f0562f5d92fd726479
brown <wangjiang@datagrand.com>
2020-01-28 01:28:23 +0800

    增强number_utils鲁棒性

--- music_focus/utils/number_utils.py
+++ music_focus/utils/number_utils.py
@@ -3,5 +3,7 @@ def parse_number(number_str):
         return int(float(number_str[:-1]) * 10e4)
     elif number_str.endswith('亿'):
         return int(float(number_str[:-1]) * 10e8)
-    else:
+    elif number_str.isdigit():
         return int(number_str)
+    else:
+        raise ValueError('param number_str: {} is invalid!'.format(number_str))
