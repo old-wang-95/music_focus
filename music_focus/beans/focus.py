5cba5fa7ab0dcf011efdc6d9c051fc77c5957c0c
brown <wangjiang@datagrand.com>
2020-01-28 01:51:24 +0800

    将focus bean中today_read变量改成recent_read

--- music_focus/beans/focus.py
+++ music_focus/beans/focus.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 class Focus:
     title: str
     description: str
-    today_read: int  # 今日阅读数
+    recent_read: int  # 最近阅读(三天内)
     read_cnt: int  # 总阅读
     discuss_cnt: int  # 总讨论数
     member_cnt: int  # 总参与人数
