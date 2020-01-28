3b751a2d48d2f910fd8320ee09f15328f6d97a7c
brown <wangjiang@datagrand.com>
2020-01-28 17:16:53 +0800

    完成transform_focuses_result的Processor的编写和测试

--- music_focus/processors/transform_focuses_result_processor.py
+++ music_focus/processors/transform_focuses_result_processor.py
@@ -4,4 +4,19 @@ from music_focus.processors.processor_base import ProcessorBase
 class TransformFocusesResultProcessor(ProcessorBase):
 
     def run(self, workflow_input, tmp_result, workflow_output):
-        pass
+        focuses_result = workflow_input['focuses']
+        json_result = {}
+        for music_type, each_focuses in focuses_result['focuses'].items():
+            json_result[music_type] = []
+            for idx, each_focus in enumerate(each_focuses):
+                json_result[music_type].append({
+                    'title': each_focus.title,
+                    'description': each_focus.description,
+                    'recent_read': each_focus.recent_read,
+                    'read_cnt': each_focus.read_cnt,
+                    'discuss_cnt': each_focus.discuss_cnt,
+                    'member_cnt': each_focus.member_cnt,
+                    'link': each_focus.link,
+                    'score': focuses_result['scores'][music_type][idx]
+                })
+        workflow_output['result'] = json_result
--- tests/test_processors/test_transform_focuses_result_processor.py
+++ tests/test_processors/test_transform_focuses_result_processor.py
@@ -1,4 +1,8 @@
+import json
 import unittest
+
+from music_focus.beans.focus import Focus
+from music_focus.processors.transform_focuses_result_processor import TransformFocusesResultProcessor
 
 
 class Test(unittest.TestCase):
@@ -7,7 +11,24 @@ class Test(unittest.TestCase):
         pass
 
     def test(self):
-        pass
+        workflow_input = {
+            'focuses': {
+                'scores': {
+                    'rock': [100, 200]},
+                'focuses': {
+                    'rock': [
+                        Focus(title='#老王乐队#', description='', recent_read=100, read_cnt=1, discuss_cnt=1, member_cnt=1,
+                              link=''),
+                        Focus(title='#新裤子乐队#', description='', recent_read=200, read_cnt=2, discuss_cnt=2, member_cnt=2,
+                              link=''),
+                    ]
+                }
+            }
+        }
+        workflow_output = {}
+        p = TransformFocusesResultProcessor()
+        p.run(workflow_input, {}, workflow_output)
+        print(json.dumps(workflow_output['result'], ensure_ascii=False, indent=2))
 
     def tearDown(self):
         pass
