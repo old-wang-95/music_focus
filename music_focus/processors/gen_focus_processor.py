b92ee892fc033e71a16f87c7d361aa5763ec90c4
brown <wangjiang@datagrand.com>
2020-01-28 16:13:41 +0800

    完成gen_focus_processor的编写和测试

--- music_focus/processors/gen_focus_processor.py
+++ music_focus/processors/gen_focus_processor.py
@@ -1,7 +1,33 @@
+import time
+
+from music_focus import logger
+from music_focus.api import weibo_api
+from music_focus.conf.users import config as users_config
 from music_focus.processors.processor_base import ProcessorBase
 
 
 class GenFocusProcessor(ProcessorBase):
 
     def run(self, workflow_input, tmp_result, workflow_output):
-        pass
+        new_focuses = {}
+        for music_type, users in users_config.items():
+            new_focuses[music_type] = []
+            for user_id, _ in users:
+                retry_time = 0
+                while retry_time <= 3:
+                    if retry_time > 0:
+                        logger.info('start to retry, current retry time is: {}'.format(retry_time))
+                    try:
+                        use_cache = False if retry_time else True
+                        user = weibo_api.get_user_info(user_id, use_cache)
+                        focuses = weibo_api.get_focuses_by_user(user, use_cache)
+                        new_focuses[music_type].extend(focuses)
+                        logger.info('fetch user: {} data success'.format(user_id))
+                        break
+                    except Exception as e:
+                        logger.exception('fetch user: {} data error! {}'.format(user_id, e))
+                        retry_time += 1
+                time.sleep(1)
+            # distinct focuses
+            new_focuses[music_type] = list({focus.title: focus for focus in new_focuses[music_type]}.values())
+        tmp_result['focuses'] = new_focuses
---
+++ tests/test_processors/test_gen_focuses_processor.py
@@ -0,0 +1,22 @@
+import unittest
+
+from music_focus.processors.gen_focus_processor import GenFocusProcessor
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test(self):
+        p = GenFocusProcessor()
+        tmp_result = {}
+        p.run({}, tmp_result, {})
+        print(tmp_result['focuses'])
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
