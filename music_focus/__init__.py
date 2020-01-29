69b76061e912017e92ab23459f24dfb3f412ec33
brown <wangjiang@datagrand.com>
2020-01-29 15:07:23 +0800

    增加文件handler, 将log输出到文件

--- music_focus/__init__.py
+++ music_focus/__init__.py
@@ -1,10 +1,18 @@
-import logging
+import logging.handlers
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
+
 handler = logging.StreamHandler()
 handler.setLevel(logging.DEBUG)
+file_handler = logging.handlers.TimedRotatingFileHandler('./log/root.log', 'D', 1, 7)
+file_handler.setLevel(logging.DEBUG)
+
 formatter = logging.Formatter(
     '%(asctime)s - %(filename)s:%(lineno)s - %(funcName)s() - %(levelname)s - %(message)s')
+
 handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
 logger.addHandler(handler)
+logger.addHandler(file_handler)
