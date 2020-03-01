f789f735bede7da8e90cbb9818e9a8dbaa4281f8
brown <wangjiang@datagrand.com>
2020-03-01 19:17:02 +0800

    删除源码里的drivers目录及程序

--- music_focus/api/chrome_api.py
+++ music_focus/api/chrome_api.py
@@ -5,15 +5,9 @@ from selenium import webdriver
 
 
 def find_elements_in_page(url, css_selector, wait_time=5):
-    if sys.platform.startswith('darwin'):
-        driver_file = 'music_focus/drivers/chromedriver_mac64'
-    elif sys.platform.startswith('linux'):
-        driver_file = 'music_focus/drivers/chromedriver_linux64'
-    else:
-        raise SystemError('can not recognize sys.platform: {}, must be darwin or linux!'.format(sys.platform))
     options = webdriver.ChromeOptions()
     options.headless = True
-    driver = webdriver.Chrome(driver_file, options=options)
+    driver = webdriver.Chrome(options=options)
 
     driver.get(url)
     time.sleep(wait_time)
