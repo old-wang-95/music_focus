a39ddffd8a2730288ffbf6fdef5c5dbdba7f2c1e
brown <wangjiang@datagrand.com>
2020-03-15 15:03:06 +0800

    修复了多进程同时删除可能导致的文件不存在报错

--- music_focus/utils/os_utils.py
+++ music_focus/utils/os_utils.py
@@ -5,7 +5,10 @@ import shutil
 def empty_dir(dir_name):
     for fname in os.listdir(dir_name):
         fpath = '{}/{}'.format(dir_name, fname)
-        if os.path.isdir(fpath):
-            shutil.rmtree(fpath)
-        else:
-            os.remove(fpath)
+        try:
+            if os.path.isdir(fpath):
+                shutil.rmtree(fpath)
+            else:
+                os.remove(fpath)
+        except FileNotFoundError:  # 防止多进程同时删除时可能的报错
+            pass
