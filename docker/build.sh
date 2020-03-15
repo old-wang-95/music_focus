e63ad6cb39259fdb1bce28a2e0619ca45124a424
brown <wangjiang@datagrand.com>
2020-03-15 23:24:55 +0800

    镜像名加上commit id

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,6 @@
 cd ..
 dt=`date '+%Y%m%d_%H%M%S'`
-docker build -f docker/Dockerfile -t music_focus:$dt .
-docker tag music_focus:$dt music_focus:latest
+commit_id=`git log -1 | head -1 | awk '{print substr($2,1,8)}'`
+img_name=music_focus:${dt}_${commit_id}
+docker build -f docker/Dockerfile -t $img_name .
+docker tag $img_name music_focus:latest
