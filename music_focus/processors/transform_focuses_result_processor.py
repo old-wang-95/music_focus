01ca67c4fd0d70f06577f3c614c7e1e585622173
brown <wangjiang@datagrand.com>
2020-03-15 16:16:12 +0800

    热点online支持黑名单

--- music_focus/processors/transform_focuses_result_processor.py
+++ music_focus/processors/transform_focuses_result_processor.py
@@ -1,3 +1,4 @@
+from music_focus.conf.focus_blacklist import config  as blacklist
 from music_focus.processors.processor_base import ProcessorBase
 
 
@@ -7,8 +8,11 @@ class TransformFocusesResultProcessor(ProcessorBase):
         focuses_result = workflow_input['focuses']
         json_result = {}
         for music_type, each_focuses in focuses_result['focuses'].items():
+            cur_black_list = blacklist['all'] + blacklist[music_type]
             json_result[music_type] = []
             for idx, each_focus in enumerate(each_focuses):
+                if each_focus.title in cur_black_list:
+                    continue
                 json_result[music_type].append({
                     'title': each_focus.title,
                     'description': each_focus.description,
